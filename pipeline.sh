3d8efb0904f53aacbabaabdd9a7043e4b2c29da4
Saluja <asaluja@testpipeline-246245.slc01.dev.ebayc3.com>
2014-08-01 15:50:16 +0000

    changed small bug in pipeline script

--- pipeline.sh
+++ pipeline.sh
@@ -7,7 +7,7 @@ wordVecs=$3
 python ${scriptsLoc}/learning.py -n -j 8 -r lars $wordVecs ${workingLoc}/adj_nn.params < ${workingLoc}/adj_nn.training
 python ${scriptsLoc}/learning.py -n -j 8 -r lars $wordVecs ${workingLoc}/nn_nn.params < ${workingLoc}/nn_nn.training
 
-python ${scriptsLoc}/compute_composed.py -a $wordVecs ${workingLoc}/adj_nn.params ${workingLoc}/nn_nn.params < ${workingLoc}/all.bigrams.tagged > ${workingLoc}/additive.bigram.reps
-python ${scriptsLoc}/compute_composed.py -m $wordVecs ${workingLoc}/adj_nn.params ${workingLoc}/nn_nn.params < ${workingLoc}/all.bigrams.tagged > ${workingLoc}/multiplicative.bigram.reps
-python ${scriptsLoc}/compute_composed.py $wordVecs ${workingLoc}/adj_nn.params ${workingLoc}/nn_nn.params < ${workingLoc}/all.bigrams.tagged > ${workingLoc}/learned.bigram.reps
+python ${scriptsLoc}/compute_composed.py -a $wordVecs ${workingLoc}/adj_nn.params ${workingLoc}/nn_nn.params < ${workingLoc}/all.titles.tagged > ${workingLoc}/additive.bigram.reps
+python ${scriptsLoc}/compute_composed.py -m $wordVecs ${workingLoc}/adj_nn.params ${workingLoc}/nn_nn.params < ${workingLoc}/all.titles.tagged > ${workingLoc}/multiplicative.bigram.reps
+python ${scriptsLoc}/compute_composed.py $wordVecs ${workingLoc}/adj_nn.params ${workingLoc}/nn_nn.params < ${workingLoc}/all.titles.tagged > ${workingLoc}/learned.bigram.reps
 
